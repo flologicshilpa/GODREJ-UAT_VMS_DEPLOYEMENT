a82558c5681652cef3dda0a1bc06ef9176ab1078
flologicshilpa <40564281+flologicshilpa@users.noreply.github.com>
2018-12-25 11:22:02 +0530

    svae changes

--- bot.js
+++ bot.js
@@ -215,7 +215,7 @@ bot.dialog('endConversationDialog',[
                 var msg = new builder.Message()
                 .addAttachment(card)
                 session.send(msg);
-        session.send("You have stopped current conversation! that is okay,just ping me when you are ready and we can chat again.")              
+        session.send("You have stopped current conversation! that is okay, just ping me when you are ready and we can chat again.")              
        // session.send('I am sorry I did not understand your question. Please retry the query or you may startover by clicking the start over button');        
         session.endDialog();
     }
