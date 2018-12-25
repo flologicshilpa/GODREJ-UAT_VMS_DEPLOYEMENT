19d44bf39d32b53d662b5396c652174737af416b
flologicshilpa <40564281+flologicshilpa@users.noreply.github.com>
2018-12-25 11:07:16 +0530

    commit change

--- bot.js
+++ bot.js
@@ -106,18 +106,18 @@ bot.dialog('GreetingDialog',[
         var myDate = new Date();
         var hrs = myDate.getHours();
     
-        var greet;
+//         var greet;
     
-        if (hrs < 12)
-            greet = 'Good Morning';
-        else if (hrs >= 12 && hrs <= 17)
-            greet = 'Good Afternoon';
-        else if (hrs >= 17 && hrs <= 24)
-            greet = 'Good Evening';
+//         if (hrs < 12)
+//             greet = 'Good Morning';
+//         else if (hrs >= 12 && hrs <= 17)
+//             greet = 'Good Afternoon';
+//         else if (hrs >= 17 && hrs <= 24)
+//             greet = 'Good Evening';
 
         session.conversationData[GlobalADID]=id;        
         session.conversationData[GloabalIntent] = intent.intent;       
-        session.send('%s  %s! Welcome to Vendor Bot.',greet,name);
+        session.send('Hello  %s! Welcome to Vendor Bot.',name);
 
    var card = {  
        
