a68230875a94ba0b8050afcbcc806670a90a1c46
flologicshilpa <40564281+flologicshilpa@users.noreply.github.com>
2019-01-25 10:21:55 +0530

    dfgfdg

--- bot.js
+++ bot.js
@@ -3448,11 +3448,11 @@ function getstatusURL(session,status)
 
 
 function createFamilyItem(BotId,BotName,ConversationId,UserId,UserName,UserQuery,UserResponse)  {
-   var date = new Date;
+   //var date = new Date;
    // var currentdate=date.toString("yyyy/MM/dd");
     
     var datetime = new Date().getTime();
-    var currentDate = datetime.toISOString()
+    var currentDate = datetime.toISOString();
     var createdid = BotName + "|"+ UserId + "|" + datetime;
  
     var documentDefinition = {"id":createdid,    
